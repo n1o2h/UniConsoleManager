16fc54177f1fe68308aaee22e7357732b3ba6a0f
n1o2h <aythmadnhylt@gmail.com>
2025-12-27 21:56:39 +0100

    creation de la classe d'exeption

---
+++ src/Exception/ValidationException.php
@@ -0,0 +1,18 @@
+<?php
+namespace App\Exception;
+
+class ValidationException extends \Exception
+{
+private array $errors;
+
+public function __construct(string $message, array $errors = [], int $code = 0, ?\Throwable $previous = null)
+{
+parent::__construct($message, $code, $previous);
+$this->errors = $errors;
+}
+
+public function getErrors(): array
+{
+return $this->errors;
+}
+}
